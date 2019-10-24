3cb795b51ecef47766be76a355f20b75743498dd
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2019-10-24 15:04:49 -0700

    Code cleanup (#4)
    * delete unused/in-progress features
    * read/write to VERSION file
    * test pip installs in CI/CD

--- containers/docker-dataops/meltano.Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM meltano/meltano
-
-RUN meltano --version
--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ import os
 from pathlib import Path
 
 detected_version = None
+version_filepath = "VERSION"
 
 if "VERSION" in os.environ:
     detected_version = os.environ["VERSION"]
     if "/" in detected_version:
         detected_version = detected_version.split("/")[-1]
-if not detected_version and os.path.exists("slalom/dataops/VERSION"):
-    detected_version = Path("slalom/dataops/VERSION").read_text()
-    if len(ver.split(".")) <= 2:
+if not detected_version and os.path.exists(version_filepath):
+    detected_version = Path(version_filepath).read_text()
+    if len(detected_version.split(".")) <= 2:
         if "BUILD_NUMBER" in os.environ:
-            detected_version = f"{ver}.{os.environ['BUILD_NUMBER']}"
+            detected_version = f"{detected_version}.{os.environ['BUILD_NUMBER']}"
 if not detected_version:
     raise RuntimeError("Error. Could not detect version.")
 
 detected_version = detected_version.lstrip("v")
 print(f"Detected version: {detected_version}")
+Path(version_filepath).write_text(f"v{detected_version}")
 
 setup(
     name="slalom.dataops",
@@ -30,6 +32,8 @@ setup(
     url="https://bitbucket.org/slalom-consulting/dataops-tools/",
     download_url="https://github.com/slalom-ggp/dataops-tools/archive/v_0.1.tar.gz",
     keywords=["DATAOPS", "SLALOM", "DATA", "AUTOMATION", "CI/CD", "DEVOPS"],
+    package_data={'': [version_filepath]},
+    include_package_data=True,
     install_requires=[
         "fire",
         "joblib",
@@ -39,11 +43,11 @@ setup(
         "xmlrunner",
     ],
     extras_require={
-        "adl": ["azure"],
-        "aws": ["awscli", "s3fs"],
-        "pandas": ["pandas"],
-        "spark": ["pyspark"],
-        "docker": ["docker"],
+        "Azure": ["azure"],
+        "AWS": ["awscli", "s3fs"],
+        "Pandas": ["pandas"],
+        "Spark": ["pyspark"],
+        "Docker": ["docker"],
     },
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
