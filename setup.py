332f9dc0fbc29564f21ecb0d3414986d0de93b40
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2019-10-24 12:50:34 -0700

    auto-publish to PyPi when GitHub releases tagged (#3)

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from distutils.core import setup
+import os
+from pathlib import Path
+
+detected_version = None
+
+if "VERSION" in os.environ:
+    detected_version = os.environ["VERSION"]
+    if "/" in detected_version:
+        detected_version = detected_version.split("/")[-1]
+if not detected_version and os.path.exists("slalom/dataops/VERSION"):
+    detected_version = Path("slalom/dataops/VERSION").read_text()
+    if len(ver.split(".")) <= 2:
+        if "BUILD_NUMBER" in os.environ:
+            detected_version = f"{ver}.{os.environ['BUILD_NUMBER']}"
+if not detected_version:
+    raise RuntimeError("Error. Could not detect version.")
+
+detected_version = detected_version.lstrip("v")
+print(f"Detected version: {detected_version}")
 
 setup(
     name="slalom.dataops",
     packages=["slalom.dataops"],
-    version="0.1",
+    version=detected_version,
     license="MIT",
     description="Slalom GGP libary for DataOps automation",
     author="AJ Steers",
